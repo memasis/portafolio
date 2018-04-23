1e657af3100b9f887b1a27aa16dbfa12504c04bd
memasis_crux <memasis@cruxconsultores.com>
2018-04-23 16:34:07 -0600

    Respaldo con servicio json creado

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -3,13 +3,15 @@ import { Http } from "@angular/http";
 
 @Injectable()
 export class InformacionService {
-
+info:any = {};
+cargada:boolean = false;
   constructor( public http:Http ) { 
  
     this.http.get("assets/data/info.pagina.json")
     
     .subscribe(data =>{
       console.log(data.json());
+      this.info = data.json();
     })
      
 }
