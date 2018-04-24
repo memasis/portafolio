587282111a33f4ba8ee9af7fdb457eb1784487de
memasis_crux <memasis@cruxconsultores.com>
2018-04-24 11:07:39 -0600

    Respaldo 24 de abril a las 11:07 am

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -5,15 +5,32 @@ import { Http } from "@angular/http";
 export class InformacionService {
 info:any = {};
 cargada:boolean = false;
+equipo:any[] = [];
+cargada_sobre_nosotros:boolean = false;
   constructor( public http:Http ) { 
  
-    this.http.get("assets/data/info.pagina.json")
-    
-    .subscribe(data =>{
-      console.log(data.json());
-      this.info = data.json();
-    })
+    this.carga_info();
+    this.carga_sobre_nosotros();
      
+}
+
+public carga_info(){
+  this.http.get("assets/data/info.pagina.json")
+    
+  .subscribe(data =>{
+    //console.log(data.json());
+    this.cargada = true;
+    this.info = data.json();
+  });
+}
+public carga_sobre_nosotros(){
+  this.http.get("https://paginaweb-a0596.firebaseio.com/equipo.json")
+    
+  .subscribe(data =>{
+    console.log(data.json());
+    this.cargada_sobre_nosotros = true;
+    this.equipo = data.json();
+  });
 }
 
 }
