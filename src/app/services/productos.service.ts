c770760446cc43c6c829a51f367ce85c30031cf0
memasis_crux <memasis@cruxconsultores.com>
2018-04-24 14:29:38 -0600

    Respaldo

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -4,6 +4,27 @@ import { Http } from '@angular/http';
 @Injectable()
 export class ProductosService {
 
-  constructor() { }
+  productos:any[] = [];
+  cargando:boolean = true;
+
+  constructor( private http:Http ) { 
+
+    this.cargar_productos();
+
+  }
+
+  public cargar_productos(){
+
+    this.cargando = true;
+
+    this.http.get('https://paginaweb-a0596.firebaseio.com/productos_idx.json')
+    
+    .subscribe(res =>{
+      console.log( res.json() );
+      this.cargando = false;
+  })
+
+
+}
 
 }
