b1468b49693a8287cc4ed5e42a85f7b8ae73a27b
memasis_crux <memasis@cruxconsultores.com>
2018-04-24 14:58:26 -0600

    Respaldo

--- src/app/components/portafolio/portafolio.component.ts
+++ src/app/components/portafolio/portafolio.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { InformacionService } from '../../services/informacion.service';
+import { ProductosService } from '../../services/productos.service';
+
 @Component({
   selector: 'app-portafolio',
   templateUrl: './portafolio.component.html',
@@ -7,7 +10,7 @@ import { Component } from '@angular/core';
 })
 export class PortafolioComponent {
 
-  constructor() { }
+  constructor( private _ps:ProductosService) { }
 
 
 }
